b9a15bd105d51ce6cf6fda7a7d20a43231d06a38
hugihlynsson <hugihlynsson@gmail.com>
2019-10-23 20:01:03 +0000

    Use <Html> from NextJS in _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class extends Document {
   render() {
     return (
-      <html lang="is">
+      <Html lang="is">
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
