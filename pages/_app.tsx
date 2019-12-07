ee0970e2be31aaefd23c3e9693cd4b14ddcbe02b
hugihlynsson <hugihlynsson@gmail.com>
2019-12-07 16:49:06 +0000

    Add GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,15 +18,27 @@ export default class CustomApp extends App {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
 
-            gtag('config', 'UA-143527780-1');
-          `,
+                gtag('config', 'UA-143527780-1');
+              `,
             }}
           />
-          
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-W79GHF3');
+              `,
+            }}
+          />
+
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -43,14 +55,6 @@ export default class CustomApp extends App {
               `,
             }}
           />
-          <noscript>
-            <img 
-              height="1" 
-              width="1" 
-              style={{ display: 'none'}} 
-              src="https://www.facebook.com/tr?id=432333127445131&ev=PageView&noscript=1"
-            />
-          </noscript>
         </Head>
 
         <Component {...pageProps} />
@@ -60,6 +64,21 @@ export default class CustomApp extends App {
           defer
           src="//static.cdn.prismic.io/prismic.js?repo=yuzu&new=true"
         />
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=432333127445131&ev=PageView&noscript=1"
+          />
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W79GHF3"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
 
         <style jsx global>{`
           @font-face {
