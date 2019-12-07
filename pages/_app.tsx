456a177796842389a42c50a524a2584a17d3b286
Hugi Hlynsson <hugihlynsson@gmail.com>
2019-12-07 16:15:25 +0000

    Fix style not being an object

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,7 +47,7 @@ export default class CustomApp extends App {
             <img 
               height="1" 
               width="1" 
-              style="display:none" 
+              style={{ display: 'none'}} 
               src="https://www.facebook.com/tr?id=432333127445131&ev=PageView&noscript=1"
             />
           </noscript>
